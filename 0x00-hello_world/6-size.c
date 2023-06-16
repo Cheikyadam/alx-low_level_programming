9f024591a0d09fa5806ee8d9f745e8e09d007430
Cheick <urbaingande@gmail.com>
2023-06-15 17:14:06 -0700

    sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Size of variables
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n",sizeof(char),sizeof(int),sizeof(long int),sizeof(long long int),sizeof(float));
+return (0);
+}
