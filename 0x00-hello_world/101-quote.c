280ee2edd3e6990ecd8f9c35fdb27cc7180dc2a0
Cheick <urbaingande@gmail.com>
2023-06-15 19:25:38 -0700

    intel syntax

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing in stderror
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+stderr->puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
