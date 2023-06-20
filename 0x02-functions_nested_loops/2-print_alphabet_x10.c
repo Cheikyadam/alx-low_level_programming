1cfef3cd0c94e459b5a5ac2c6ea9b13ab1d4773c
Cheick <urbaingande@gmail.com>
2023-06-20 07:22:13 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabetx10 - printing alphabets ten times
+ *
+ *section header : including main.h
+ *Return: void
+ */
+
+void print_alphabet_x10()
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+print_alphabet();
+}
+}
