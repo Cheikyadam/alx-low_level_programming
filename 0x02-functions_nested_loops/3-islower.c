8ea9f5a1900fb79ece5bfa5cb9409ace75f4da7d
Cheick <urbaingande@gmail.com>
2023-06-20 12:44:07 -0700

    task 3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_islower -determine casse
+ *
+ *section header : including main.h
+ *Return: int 0 or 1
+ */
+
+int _islower(int c)
+{
+char p;
+
+for (p = 'a'; p <= 'z'; p++)
+{
+if (p == c)
+return (1);
+}
+return (0);
+}
+
