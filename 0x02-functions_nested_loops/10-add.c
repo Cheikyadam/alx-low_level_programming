655b0302dda7eb23bb3693293b31532caab50371
Cheick <urbaingande@gmail.com>
2023-06-21 13:36:16 -0700

    task 10 and 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adding
+ * @a: the number 1
+ * @b: the number 2
+ *
+ * section header : including main.h
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - timetable
+ *
+ * section header : including main.h
+ * Return: void
+ */
+
+void times_table(void)
+{
+int i;
+int j;
+int p;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+p = i * j;
+if (p < 10)
+{
+_putchar(48 + p);
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+else
+{
+_putchar(48 + ((p - (p % 10)) / 10));
+_putchar(48 + (p % 10));
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');  
+}
+}
+}
+_putchar('\n');
+}
+}
