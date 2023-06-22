f10811e36f2b44705aa1256404ce908ebb5a6fe0
Cheick <urbaingande@gmail.com>
2023-06-22 12:58:01 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - to know if is upper
+ * @c: the caracter to check
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+return (1);
+else
+return (0);
+}
