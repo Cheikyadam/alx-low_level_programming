18c9f841a4ae767fca1243164e138f64666368ac
Cheick <urbaingande@gmail.com>
2023-06-22 17:44:44 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonale
+ * @n: times
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i - 1; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
