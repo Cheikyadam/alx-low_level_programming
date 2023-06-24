66a12df3d8f935e2174e22851f8e3d5ea640613e
Cheick <urbaingande@gmail.com>
2023-06-24 05:46:32 -0700

    prime factor u

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,22 +12,21 @@ long int i;
 long int it;
 long int it2;
 long int j;
-long int prime; 
+long int prime;
 
 it = 612852475143 / 2;
 for (i = 2; i <= it; i++)
 {
- if ((612852475143 % i) == 0)
+if ((612852475143 % i) == 0)
 {
- it2 = i / 2;
- j = 2;
- while(j <= it2 && (i % j) != 0)
-   j++;
- if (j > it2)
-   prime = i;
+it2 = i / 2;
+j = 2;
+while (j <= it2 && (i % j) != 0)
+j++;
+if (j > it2)
+prime = i;
 }
 }
-
 printf("%ld", prime);
 printf("\n");
 return (0);
