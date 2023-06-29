d1d205278ecc3cd1d9a1eafcb522fb7681d594f3
Cheick <urbaingande@gmail.com>
2023-06-29 11:24:30 -0700

    task 2 betty correct

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,9 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (j = 0; (j < n) && (src[j] != '\0'); j++)
 		dest[j] = src[j];
-	
+
 	for ( ; j < n; j++)
-		dest[j] = '\0';	
+		dest[j] = '\0';
 
 	return (dest);
 }
