aa782fff60b4e064e8f645b46335d4e5b4b6ad36
Cheick <urbaingande@gmail.com>
2023-06-29 19:57:15 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - coding 
+ * @src: source
+ *
+ * Return: char *
+ */
+
+char *leet(char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (src[i] == 'a' || src[i] == 'A' || src[i] == 'e' ||
+		src[i] == 'E' || src[i] == 'o' || src[i] == 'O'
+		|| src[i] == 't' || src[i] == 'T' ||  src[i] == 'l'
+		|| src[i] == 'L')
+		{
+		}
+	}
+	return (src);
+}
