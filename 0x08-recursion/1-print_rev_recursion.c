b5bcdfc4de921c97d780d3d5e2b7608c8e70b973
Cheick <urbaingande@gmail.com>
2023-07-06 14:46:13 -0700

    print rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * helper - to help
- * @s: the string
- *
- * Return: Nothing
- */
-
-void helper(char *s)
-{	
-	if (*s == '\0')
-		;
-	else
-	{
-		s++;
-		helper(s);
-		/*if (*s == '\\' && *(s + 1) == 'n')
-			_putchar('\n');
-		else
-			_putchar(*s);*/
-		_putchar(*s);
-	}
-}
-
-
-/**
  * _print_rev_recursion  - reversing a string
  * @s: the string
  *
@@ -33,8 +9,9 @@ void helper(char *s)
 
 void _print_rev_recursion(char *s)
 {
-	char *p = s;
-	
-	helper(s);
-	_putchar(*p);
+	if (*s !=  '\0')
+	{
+		_print_rev_recursion((s + 1));
+		_putchar(*s);
+	}
 }
