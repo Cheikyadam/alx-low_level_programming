98e3eabc8f337649ce6bb87a6745515a3cd0f8d7
Cheick <urbaingande@gmail.com>
2023-07-11 11:21:56 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocation grid
+ * @width: the width of the grid
+ * @height: the height of the grid
+ *
+ * Return: an double array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **tab;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	tab = malloc(sizeof(int *) * width);
+	if (tab == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		tab[i] = malloc(sizeof(int) * height);
+		if (tab[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < width; i++)
+		for (j = 0; j < width; j++)
+			tab[i][j] = 0;
+	return (tab);
+}
