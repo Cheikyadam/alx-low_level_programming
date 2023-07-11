a92f8192f690951bb60849b78ffb7df81b82a7e7
Cheick <urbaingande@gmail.com>
2023-07-11 05:22:44 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * lenght - the lenght of a string
+ * @s:the string
+ *
+ * Return: an integer
+ */
+
+int lenght(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * str_concat - concatenation of strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: char *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new;
+	int i;
+	int j;
+
+	if (s1 == NULL)
+		return (s2);
+	if (s2 == NULL)
+		return (s1);
+	new = malloc(sizeof(char) * (lenght(s1) + 1 + lenght(s2)));
+	if (new != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			new[i] = s1[i];
+		new[i] = ' ';
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			i++;
+			new[i] = s2[j];
+		}
+	}
+	return (new);
+}
