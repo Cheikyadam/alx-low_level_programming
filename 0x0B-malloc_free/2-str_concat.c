e4e5c56cf34852a8e80bf6fbabb807d18c4932a8
Cheick <urbaingande@gmail.com>
2023-07-11 05:55:29 -0700

    task 2 fixing issues

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,8 @@ int lenght(char *s)
 {
 	int i;
 
+	if (s == NULL)
+		return (0);
 	for (i = 0; s[i] != '\0'; i++)
 		;
 	return (i);
@@ -31,9 +33,21 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
-	new = malloc(sizeof(char) * (lenght(s1) + 1 + lenght(s2)));
+	new = malloc(sizeof(char) * (lenght(s1) + lenght(s2)));
 	if (new != NULL)
 	{
+		if (s2 == NULL)
+		{
+			for (i = 0; s1[i] != '\0'; i++)
+				new[i] = s1[i];
+			return (new);
+		}
+		if (s1 == NULL)
+		{
+			for (i = 0; s2[i] != '\0'; i++)
+				new[i] = s2[i];
+			return (new);
+		}
 		for (i = 0; s1[i] != '\0'; i++)
 			new[i] = s1[i];
 		for (j = 0; s2[j] != '\0'; j++)
