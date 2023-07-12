d4e2728f128b50b1c3cdf35b17f40ab518980603
Cheick <urbaingande@gmail.com>
2023-07-11 18:07:36 -0700

    advanced 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * lenght - the lenght of a string
+ * @s:the string
+ *
+ * Return: an integer
+ */
+
+int lenght(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * argstostr - concatenation of arguments
+ * @ac: number of arguments
+ * @av: the arguments
+ *
+ * Return: char *
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *new;
+	int i;
+	int j;
+	int k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		k += lenght(av[i]);
+
+	new = malloc(sizeof(char) * k);
+	k = 0;
+	if (new != NULL)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; av[i][j] != '\0'; j++)
+			{
+				new[k] = av[i][j];
+				k++;
+			}
+			new[k] = '\n';
+			k++;
+		}
+	}
+	return (new);
+}
