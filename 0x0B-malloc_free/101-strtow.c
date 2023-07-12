5b98061c697096e38684ac96a94d8fc280d7e021
Cheick <urbaingande@gmail.com>
2023-07-11 18:24:14 -0700

    advanced 101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - spliting a string
+ * @str: the string
+ *
+ * Return: char *
+ */
+
+char **strtow(char *str)
+{
+
+
+	if (str == NULL || str == '')
+		return (NULL);
+}
