dda70fe1225bc2c480b7b62a71fa0de91400433b
Cheick <urbaingande@gmail.com>
2023-07-17 15:41:10 -0700

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Learning structures and how to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _MYDOG_
+#define _MYDOG_
+/**
+ * struct dog - my dog description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
