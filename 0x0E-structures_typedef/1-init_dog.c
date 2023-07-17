d730df50060adb4f94f5f870d575816395b7f5dc
Cheick <urbaingande@gmail.com>
2023-07-17 16:09:10 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initilization off my dog
+ * @d: the dog
+ * @name: his name
+ * @age: his age
+ * @owner: the owner
+ *
+ * Return a dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	int i;
+
+	d->name = malloc(sizeof(name));
+	for (i = 0; name[i] != '\0'; i++)
+		d->name[i] = name[i];
+	d->owner = malloc(sizeof(owner));
+	for (i = 0; owner[i] != '\0'; i++)
+		d->owner[i] = owner[i];
+	d->age = age;
+}
