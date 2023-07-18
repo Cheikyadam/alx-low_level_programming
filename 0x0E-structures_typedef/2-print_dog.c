be25c53213eabcfff07a0d93314f5b773fee3575
Cheick <urbaingande@gmail.com>
2023-07-18 03:22:20 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,10 +23,14 @@ void print_dog(struct dog *d)
 		}
 		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
+		{
 			printf("Owner: %s\n", d->owner);
+
+		}
 		else
+		{
 			printf("(nil)\n");
+		}
 	}
-	else
-		printf("\n");
+
 }
