511ff00a55d1b3ef5ee45998356d822fb3eb6bd2
Cheick <urbaingande@gmail.com>
2023-07-17 19:38:46 -0700

    4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _MY_HEADER_
+#define _MY_HEADER_
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _MY_HEADER_
+#define _MY_HEADER_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
