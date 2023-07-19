8228e0c1ace772330e3cdccaa837b6662ba59c34
Cheick <urbaingande@gmail.com>
2023-07-19 06:35:00 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,18 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || (cmp == NULL) || array == NULL)
 	{
 		return (-1);
 	}
-	if (cmp != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		if (cmp(array[i]))
 		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
 	}
 	return (-1);
