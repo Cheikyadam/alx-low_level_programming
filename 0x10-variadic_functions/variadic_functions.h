aef93b972a3e0c3967ca01cdb1b7f1d52dcecd65
Cheick <urbaingande@gmail.com>
2023-07-20 04:03:30 -0700

    task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _MY_VAR_FUNC_
+#define _MY_VAR_FUNC_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
