86f120162b8a7fb2699e63719b0766e1b3c8ff88
Cheick <urbaingande@gmail.com>
2023-07-20 18:56:19 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,28 +13,38 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	int i = 0;
+	int j = 0;
 
 	if (format != NULL)
 	{
+		while (format[j] != '\0')
+		{
+			j++;
+		}
 		va_start(ap, format);
 		while (format[i] != '\0')
 		{
 			switch (format[i])
 			{
 				case 'c':
-					printf("%c, ", va_arg(ap, int));
+					printf("%c", va_arg(ap, int));
 					break;
 				case 'i':
-					printf("%d, ", va_arg(ap, int));
+					printf("%d", va_arg(ap, int));
 					break;
 				case 'f':
-					printf("%f, ", va_arg(ap, double));
+					printf("%f", va_arg(ap, double));
 					break;
 				case 's':
-					printf("%s, ", va_arg(ap, char *));
+					printf("%s", va_arg(ap, char *));
 					break;
 				default:
 					break;
+			}
+			if (i < (j - 1) && (format[i] == 'c' || format[i] == 'i'
+			|| format[i] == 'f' || format[i] == 's'))
+			{
+				printf(", ");
 			}
 			i++;
 		}
