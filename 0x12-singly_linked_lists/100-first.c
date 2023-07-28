7e5484a9aca0c8c580def1aa8cb3f1a523f8eb30
Cheikyadam <urbaingande@gmail.com>
2023-07-28 01:44:04 +0000

    adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_first - the function
+ *
+ * Return: Nothing
+ */
+
+void __attribute__((constructor))print_first(void)
+{
+
+	printf("You\'re beat! and yet, you must
+		allow,\nI bore my house upon my back!\n");
+
+}
