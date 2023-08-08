f49f7f71cc5ee7c94111b530e1d1ae1f3fc2ca8c
Cheick <urbaingande@gmail.com>
2023-08-07 17:45:57 -0700

    init

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - reading a text file
+ * @filename: the name of the file
+ * @ letters: the numbers of letters
+ *
+ * Return: the size
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fd;
+	ssize_t n;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = fopen(filename, "r");
+	if (fd == NULL)
+		return (0);
+	n = write(0, fd, letters);
+	fclose(fd);
+	if (n == -1)
+		return (0);
+	return (n);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Here learning file manipulation
