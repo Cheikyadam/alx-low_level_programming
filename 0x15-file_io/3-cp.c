4f98bd97089278541d197bcd51855165519bf346
Cheick <urbaingande@gmail.com>
2023-08-07 20:27:30 -0700

    task 3 u

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,13 +6,14 @@
  * exit_f - to exit
  * @s: the message
  * @n: the exit code
+ * @f: the filename
  *
  * Return: Nothing
  */
 
-void exit_f(char *s, int n)
+void exit_f(char *s, char *f, int n)
 {
-	dprintf(2, "%s", s);
+	dprintf(2, "%s%s\n", s, f);
 	exit(n);
 }
 
@@ -30,22 +31,22 @@ int main(int argc, char **argv)
 	ssize_t n, n2;
 
 	if (argc != 3)
-		exit_f("Usage: cp file_from file_to\n", 97);
+		exit_f("Usage: cp file_from file_to", " ", 97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		exit_f("Error: Can't read from file %s\n", 98);
+		exit_f("Error: Can't read from file ", argv[1], 98);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (to == -1)
-		exit_f("Error: Can't write to %s\n", 99);
+		exit_f("Error: Can't write to ", argv[2], 99);
 	while ((n = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
 		n2 = write(to, buffer, n);
 		if (n2 == -1)
-			exit_f("Error: Can't write to %s\n", 99);
+			exit_f("Error: Can't write to ", argv[2], 99);
 	}
 	if (n == -1)
-		exit_f("Error: Can't read from file %s\n", 98);
+		exit_f("Error: Can't read from file ", argv[1], 98);
 	if (close(to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", to);
