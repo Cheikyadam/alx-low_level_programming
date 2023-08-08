f096aa340fced664c0c5a468234b5446d44e16ba
Cheick <urbaingande@gmail.com>
2023-08-07 20:06:37 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,20 @@
 #define BUFFER_SIZE 1024
-
 #include "main.h"
 #include <stdlib.h>
+
+/**
+ * exit_f - to exit
+ * @s: the message
+ * @n: the exit code
+ *
+ * Return: Nothing
+ */
+
+void exit_f(char *s, int n)
+{
+	dprintf(2, "%s", s);
+	exit(n);
+}
 
 /**
  * main - cp
@@ -10,7 +23,6 @@
  *
  * Return: Always 0 if no error
  */
-
 int main(int argc, char **argv)
 {
 	char buffer[BUFFER_SIZE];
@@ -18,39 +30,22 @@ int main(int argc, char **argv)
 	ssize_t n, n2;
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		exit_f("Usage: cp file_from file_to\n", 97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		exit_f("Error: Can't read from file %s\n", 98);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		exit_f("Error: Can't write to %s\n", 99);
 	while ((n = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
 		n2 = write(to, buffer, n);
 		if (n2 == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			exit_f("Error: Can't write to %s\n", 99);
 	}
 	if (n == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+		exit_f("Error: Can't read from file %s\n", 98);
 	if (close(to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", to);
