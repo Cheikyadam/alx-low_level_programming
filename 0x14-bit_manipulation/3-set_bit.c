9441072385024733325a35de2300690ad7307d26
Cheikyadam <urbaingande@gmail.com>
2023-08-31 16:02:49 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - set a bit
+ * @n: the number
+ * @index: the index
+ *
+ * Return:the bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (n == NULL)
+		return (-1);
+
+	(*n) |= (1 << index);
+	return (1);
+}
