0a67982d17dbe746b6eeae8a34395f55a7b03938
Cheikyadam <urbaingande@gmail.com>
2023-09-01 03:42:34 -0700

    task 2 err

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,35 +10,15 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int tmp = n;
-	unsigned int nb_bit = 0;
-	unsigned int m;
-
 	if (n == 0)
 	{
 		if (index == 0)
 			return (0);
 		return (-1);
 	}
-	while (tmp != 0)
-	{
-		tmp >>= 1;
-		nb_bit++;
-	}
-	nb_bit--;
 
-	if (index > nb_bit)
-		return (-1);
 
-	m = 1 << (nb_bit);
-
-	while (nb_bit != index)
-	{
-		nb_bit--;
-		m >>= 1;
-	}
-
-	if (n & m)
+	if ((n >> index) & 1)
 		return (1);
 	else
 		return (0);
