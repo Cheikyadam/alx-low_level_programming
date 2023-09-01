478b4825474adae229edc687886f50b8996cbb16
Cheikyadam <urbaingande@gmail.com>
2023-09-01 03:24:04 -0700

    task 1 er

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,9 +9,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int nb_bit = 0;
+	int nb_bit = 0;
 	unsigned long int tmp = n;
-	unsigned int h_bit = 1;
 
 	if (n == 0)
 		_putchar('0');
@@ -23,15 +22,15 @@ void print_binary(unsigned long int n)
 			nb_bit++;
 		}
 
-		h_bit  <<= (nb_bit - 1);
-
-		while (h_bit != 0)
+		nb_bit--;
+		while (nb_bit >= 0)
 		{
-			if ((n) & h_bit)
+			if (((n) >> nb_bit) & 1)
 				_putchar('1');
 			else
 				_putchar('0');
-			h_bit  >>= 1;
+
+			nb_bit--;
 		}
 	}
 }
