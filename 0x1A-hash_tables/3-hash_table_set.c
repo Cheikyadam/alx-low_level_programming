d9bab35ab3cf92e5c466e5cebd594d7b389ffdc5
Cheikyadam <urbaingande@gmail.com>
2023-12-01 08:20:54 -0800

    3 up

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,94 @@
 #include "hash_tables.h"
 
+
+/**
+ * _strcmp - comparison
+ * @s1: first string
+ * @s2: 2nd string
+ *
+ * Return: an integer
+ */
+
+int _strcmp(const char *s1, const char *s2)
+{
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+
+	while ((*s1) == (*s2) && (*s1) != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (((*s1) == '\0') && ((*s2) != '\0'))
+		return (-1);
+	if (((*s2) == '\0') && ((*s1) != '\0'))
+		return (-1);
+	if ((*s1) == '\0')
+		s1--;
+	if ((*s2) == '\0')
+		s2--;
+	return ((*s1) - (*s2));
+}
+
+/**
+ * _strdup - to duplicate a string
+ * @s: the string
+ *
+ * Return: the string
+ */
+
+char *_strdup(const char *s)
+{
+	char *d;
+
+	d = malloc(_strlen(s) + 1);
+	if (d != NULL)
+	{
+		d = _strcpy(d, s);
+		return (d);
+	}
+	return (NULL);
+}
+
+/**
+ * _strlen - lenght
+ * @s: lenght of s
+ *
+ * Return: an int
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	if (s != NULL)
+		for (i = 0; s[i] != '\0'; i++)
+			;
+	return (i);
+}
+/**
+ * _strcpy - copying
+ * @src: source
+ * @dest: destination
+ *
+ * Return: char *
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int i;
+
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+
 /**
  * hash_table_set - to add elements in the hash table
  * @ht: the hash table
@@ -19,8 +108,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 		return (0);
-	value_dup = strdup(value);
-	key_dup = strdup(key);
+	value_dup = _strdup(value);
+	key_dup = _strdup(key);
 	if (value_dup == NULL || key == NULL)
 		return (0);
 
@@ -37,7 +126,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = new;
 	else
 	{
-		if (strcmp(current->key, key) == 0)
+		if (_strcmp(current->key, key) == 0)
 			current->value = value_dup;
 		else
 		{
