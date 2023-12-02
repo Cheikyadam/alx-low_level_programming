01b6451e8e6249ec27b4a6410955665317ef7882
Cheikyadam <urbaingande@gmail.com>
2023-12-01 16:00:44 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,30 +20,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 		return (0);
 	value_dup = strdup(value);
-	key_dup = strdup(key);
-	if (value_dup == NULL || key == NULL)
+	if (value_dup == NULL)
 		return (0);
 
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	if (current != NULL && strcmp(current->key, key) == 0)
+	{
+		free(current->value);
+		ht->array[index]->value = value_dup;
+		return (1);
+	}
+	key_dup = strdup(key);
 	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	if (new == NULL || key_dup == NULL)
 		return (0);
 	new->key = key_dup;
 	new->value = value_dup;
 	new->next = NULL;
-
-	index = key_index((unsigned char *)key, ht->size);
-	current = ht->array[index];
 	if (current == NULL)
 		current = new;
 	else
 	{
-		if (strcmp(current->key, key) == 0)
-			current->value = value_dup;
-		else
-		{
-			new->next = current;
-			current = new;
-		}
+		new->next = current;
+		current = new;
 	}
 	ht->array[index] = current;
 	return (1);
